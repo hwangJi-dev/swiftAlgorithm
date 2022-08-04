2961e0009acb20ea02f5ba610329b847f1d93f7a
hwangJi-dev <melon2228@swu.ac.kr>
2022-08-05 04:02:44 +0900

    #11 - 타겟 넘버 문제 풀이

---
+++ Programmers/Programmers/level2/타겟 넘버.swift
@@ -0,0 +1,27 @@
+//
+//  타겟 넘버.swift
+//  Programmers1
+//
+//  Created by hwangJi on 2022/08/05.
+//
+
+import Foundation
+
+var answer: Int = 0
+
+func solution(_ numbers:[Int], _ target:Int) -> Int {
+    dfs(numbers: numbers, target: target, index: 0, sum: 0)
+    return answer
+}
+
+func dfs(numbers: [Int], target: Int, index: Int, sum: Int) {
+    if index == numbers.count {
+        if target == sum {
+            answer += 1
+        }
+        return
+    }
+
+    dfs(numbers: numbers, target: target, index: index + 1, sum: sum + numbers[index])
+    dfs(numbers: numbers, target: target, index: index + 1, sum: sum - numbers[index])
+}
