d3943059a32759df29c2c3044513449b9d7836bc
hwangJi-dev <melon2228@swu.ac.kr>
2022-10-11 22:23:19 +0900

    #23 - 실패율 문제 풀이

---
+++ Programmers/Programmers/level1/실패율.swift
@@ -0,0 +1,33 @@
+//
+//  실패율.swift
+//  Programmers1
+//
+//  Created by hwangJi on 2022/10/11.
+//
+
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var stageDict: [Int: Int] = [:]
+
+    for i in 1...N {
+        stageDict[i] = 0
+    }
+    
+    for i in stages {
+        stageDict[i] = stageDict[i] != nil ?  stageDict[i]! + 1 : 1
+    }
+    
+    var stageCount = stages.count
+    var failureDict: [Int: Double] = [:]
+    
+    for i in stageDict.keys.filter({ $0 <= N }).sorted() {
+        failureDict[i] = Double(stageDict[i]!)/Double(stageCount)
+        stageCount -= stageDict[i]!
+    }
+    
+    var sortedDict = failureDict.sorted { $0.key < $1.key }
+    sortedDict = sortedDict.sorted { $0.value > $1.value }
+    
+    return sortedDict.map { $0.key }
+}
