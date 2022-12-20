edbd3da8ec8de2020bb72d467bbd8978c7e50f37
hwangJi-dev <melon2228@swu.ac.kr>
2022-12-20 14:12:16 +0900

    #36 - 튜플 문제 풀이

---
+++ Programmers/Programmers/level2/튜플.swift
@@ -0,0 +1,30 @@
+//
+//  튜플.swift
+//  Programmers1
+//
+//  Created by hwangJi on 2022/12/14.
+//
+
+import Foundation
+
+func solution2(_ s:String) -> [Int] {
+    var tupleArr: [[Int]] = []
+    let s = s.trimmingCharacters(in: ["{", "}"]).components(separatedBy: "},{").filter({ $0 != "" })
+    
+    for i in s {
+        tupleArr.append(i.components(separatedBy: ",").map({ Int($0)! }))
+    }
+    
+    tupleArr = tupleArr.sorted { $0.count < $1.count }
+    var resultArr: [Int] = []
+
+    for result in tupleArr {
+        for j in result {
+            if !resultArr.contains(j) {
+                resultArr.append(j)
+            }
+        }
+    }
+    
+    return resultArr
+}
