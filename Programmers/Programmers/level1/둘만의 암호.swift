e604e2f28fa8c559a6d3f2d78c3141d3d4f87e40
hwangJi-dev <melon2228@swu.ac.kr>
2023-02-05 01:38:22 +0900

    #105 - 둘만의 암호 문제 풀이

--- Programmers/Programmers/level1/둘만의 암호.swift
+++ Programmers/Programmers/level1/둘만의 암호.swift
@@ -8,13 +8,11 @@
 import Foundation
 
 func solution(s:String, skip:String, index:Int) -> String {
-    let abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].filter{ !skip.contains($0) }
+    var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"].filter{ !skip.contains($0) }
     var ans = ""
     
     for i in s {
-        var idx = (abc.firstIndex(of: String(i))! + index) % abc.count
-        idx = idx > abc.count - 1 ? idx - abc.count : idx
-        ans += abc[idx]
+        ans += abc[(abc.firstIndex(of: String(i))! + index) % abc.count]
     }
     
     return ans
