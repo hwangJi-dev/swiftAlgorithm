5bab432553ecf1cdce745044b62f061921125147
hwangJi-dev <melon2228@swu.ac.kr>
2023-03-06 00:09:05 +0900

    #122 - 바탕화면 정리 문제 풀이

--- Programmers/Programmers/level1/바탕화면 정리.swift
+++ Programmers/Programmers/level1/바탕화면 정리.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+func solution(wallpaper:[String]) -> [Int] {
+    var coordinate = [50,50,0,0]
+    
+    for (idx, i) in wallpaper.enumerated() {
+        for (jdx, j) in i.enumerated() {
+            if j == "#" {
+                coordinate[0] = min(idx, coordinate[0])
+                coordinate[1] = min(jdx, coordinate[1])
+                coordinate[2] = max(idx + 1, coordinate[2])
+                coordinate[3] = max(jdx + 1, coordinate[3])
+            }
+        }
+    }
+    
+    return coordinate
+}
