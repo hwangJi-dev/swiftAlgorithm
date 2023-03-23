01c58a29e05b6bd10e9ee0086daedf46a16c7a9d
hwangJi-dev <melon2228@swu.ac.kr>
2023-03-24 02:05:51 +0900

    #142 - Distinct 문제 풀이

---
+++ Programmers/Codility/Sorting/Distinct.swift
@@ -0,0 +1,12 @@
+//
+//  Distinct.swift
+//  Programmers1
+//
+//  Created by hwangJi on 2023/03/24.
+//
+
+import Foundation
+
+public func solution5(_ A : inout [Int]) -> Int {
+    return Set(A).count
+}
