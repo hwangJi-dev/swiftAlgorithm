d22049d82f8fe7a98a8ce9a307509c8de9d1be23
hwangJi-dev <melon2228@swu.ac.kr>
2023-03-29 20:12:12 +0900

    #29 - 단어 변환 문제 풀이

---
+++ Programmers/Programmers/level3/단어 변환.swift
@@ -0,0 +1,8 @@
+//
+//  단어 변환.swift
+//  Programmers1
+//
+//  Created by hwangJi on 2023/03/29.
+//
+
+import Foundation
--- Programmers/main.swift
+++ Programmers/main.swift
@@ -7,9 +7,7 @@
 
 import Foundation
 if #available(macOS 13.0, *) {    
-    print(solution(n: 4, computers: [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]))
-//    print(solution(n: 6, computers: [[1, 1, 1, 0, 0, 0], [1, 1, 0, 0, 0, 0], [1, 0, 1, 1, 1, 0], [0,0,1,1,0,0], [0,0,1,0,1,0], [0,0,0,0,0,1]]))
-//    print(solution(n: 6, computers: [[1, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0,0,0,1,1,0], [0,0,0,1,1,1], [0,0,0,0,1,1]]))
+    print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
 } else {
     // Fallback on earlier versions
 }
