0249b44ba3b4f32ec6d02bce5186cc2b39326925
hwangJi-dev <melon2228@swu.ac.kr>
2023-04-13 04:54:46 +0900

    #199 - H-Index 문제 풀이

---
+++ Programmers/Programmers/level2/H-Index.swift
@@ -0,0 +1,23 @@
+//
+//  H-Index.swift
+//  Programmers1
+//
+//  Created by hwangJi on 2023/04/13.
+//
+
+import Foundation
+
+func solution(citations:[Int]) -> Int {
+    var HIndex = 0
+    
+    for i in 0..<citations.max()! {
+        let up = citations.filter({ $0 >= i }).count
+        let down = citations.filter({ $0 < i }).count
+        
+        if up >= i && down <= i {
+            HIndex = max(HIndex, i)
+        }
+    }
+    
+    return HIndex
+}
--- Programmers/main.swift
+++ Programmers/main.swift
@@ -10,6 +10,4 @@ import Foundation
 //print(solution(n: 4, costs: [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
 //print(solution(n: 6, costs: [[0,1,1],[0,2,2],[0, 3,5],[1,3,3],[2,5,4], [2,3,3], [3, 4, 6], [1, 4, 7], [4, 5, 2], [3, 5, 1]]))
 
-print(solution1(numbers: [9, 5, 34, 3, 30]))
-print(solution1(numbers: [6, 10, 2]))
-print(solution1(numbers: [0, 0, 0]))
+print(solution(citations: [3, 0, 6, 1, 5]))
