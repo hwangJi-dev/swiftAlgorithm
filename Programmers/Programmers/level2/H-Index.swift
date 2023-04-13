53eebfb900787d8d9eaa9d16350cee02752cb37b
hwangJi-dev <melon2228@swu.ac.kr>
2023-04-13 15:54:48 +0900

    #199 - H-Index 문제 풀이

--- Programmers/Programmers/level2/H-Index.swift
+++ Programmers/Programmers/level2/H-Index.swift
@@ -8,16 +8,31 @@
 import Foundation
 
 func solution(citations:[Int]) -> Int {
-    var HIndex = 0
+    let citations = citations.sorted(by: >)
+    var ans = 0
     
-    for i in 0..<citations.max()! {
-        let up = citations.filter({ $0 >= i }).count
-        let down = citations.filter({ $0 < i }).count
-        
-        if up >= i && down <= i {
-            HIndex = max(HIndex, i)
+    for i in 0..<citations.count {
+        if i + 1 <= citations[i] {
+            ans = i + 1
+        } else {
+            break
         }
     }
     
-    return HIndex
+    return ans
 }
+
+//func solution(citations:[Int]) -> Int {
+//    var HIndex = 0
+//
+//    for i in 0..<citations.max()! {
+//        let up = citations.filter({ $0 >= i }).count
+//        let down = citations.filter({ $0 < i }).count
+//
+//        if up >= i && down <= i {
+//            HIndex = max(HIndex, i)
+//        }
+//    }
+//
+//    return HIndex
+//}
